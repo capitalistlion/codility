59119d6a96939966d29f467c9988478668cad115
Panashe Madzudzo <admin@asantech.dev>
2022-02-28 11:23:41 +0200

    add MaxProductOfThree

--- sorting/MaxProductOfThree.py
+++ sorting/MaxProductOfThree.py
@@ -1,15 +1,17 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
+from functools import reduce
 import time
 import random
 
 def solution(A):
     # write your code in Python 3.6
-    return len(set(A))
+    A.sort()
+    return max(A[0]*A[1]*A[-1], A[-1]*A[-2]*A[-3])
 
 A = []
-for i in range(0,1*10**5):
-    n = random.randint(-1*10**6,1*10**6)
+for i in range(3,1*10**5):
+    n = random.randint(-1*10**5,1*10**5)
     A.append(n)
 start_time = time.time()
 print(solution(A))
@@ -18,4 +20,4 @@ total_time = end_time - start_time
 print("Time: ", total_time)
 
 #CODILITY URL
-#https://app.codility.com/demo/results/trainingPZSHXB-KX6/
+#https://app.codility.com/demo/results/trainingSVQ9ZP-QBJ/
